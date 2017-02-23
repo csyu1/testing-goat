d9516d206bd9142f1f555f70584d6a2c17b043a7
Carl <carl.earvin.yu@gmail.com>
2017-02-23 15:10:44 +0800

    Refactor home page to use template

---
+++ superlists/lists/tests.py
@@ -0,0 +1,20 @@
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.test import TestCase
+from django.template.loader import render_to_string
+
+from lists.views import home_page
+
+
+# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_homepage(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
