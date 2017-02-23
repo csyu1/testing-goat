d2df8b6e6809008a72a7d48577f3e1882674d745
Carl <carl.earvin.yu@gmail.com>
2017-02-23 15:14:37 +0800

    home page html generated from template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -23,7 +23,7 @@ class NewVisistorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1')
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
