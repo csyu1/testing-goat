b666bda62bf62f3803d6440928087bdec78e1430
Carl <carl.earvin.yu@gmail.com>
2017-02-23 13:53:29 +0800

    FT specs in comments. uses unittest

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,10 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
 
-browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
-    firefox_path='/home/carl/git/testing_goat/firefox/firefox'
-))
 
-browser.get('http://localhost:8001')
+class NewVisistorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
+            firefox_path='/home/carl/git/testing_goat/firefox/firefox'
+        ))
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8001')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Finish test!")
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
+
+
+if __name__ == "__main__":
+    unittest.main()
