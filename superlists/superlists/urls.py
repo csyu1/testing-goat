53f95e36b1644374c35e4034e4f43a929c20420e
Carl <carl.earvin.yu@gmail.com>
2017-02-24 13:00:44 +0800

    "new URL, view and template to display lists"

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from lists.views import home_page
+from lists import views
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', home_page),
+    url(r'^$', views.home_page),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
