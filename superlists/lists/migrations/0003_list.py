3fe8826081b258954b4cd5e9bd508c1824f10bfc
Carl <carl.earvin.yu@gmail.com>
2017-02-24 13:30:54 +0800

    adjust models so that items are associated with different lists

---
+++ superlists/lists/migrations/0003_list.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0002_item_text'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+            ],
+        ),
+    ]
