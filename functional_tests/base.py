8e785b63cf2fc709cb6fe7e905939ddc27ed4fea
Carl <carl.earvin.yu@gmail.com>
2017-03-03 11:23:58 +0800

    FT list item validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,14 +2,12 @@ import sys
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-from unittest import skip
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
 
-MAX_WAIT = 10
+MAX_WAIT = 3
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -36,6 +34,16 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
