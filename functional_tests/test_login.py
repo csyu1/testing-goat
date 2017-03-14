4442786247a81052f9cecc07d61c00254b7434fd
Carl <carl.earvin.yu@gmail.com>
2017-03-14 11:12:43 +0800

    Custom passwordless auth backend + custom user model

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,7 +1,7 @@
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-
 import re
+from selenium import webdriver
 
 from .base import FunctionalTest
 
@@ -11,7 +11,7 @@ SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
 
-    def test_can_get_email_link_to_login(self):
+    def test_can_get_email_link_to_log_in(self):
         # Edith goes to the awesome superlists site
         # and notices a "Log in" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
@@ -28,7 +28,7 @@ class LoginTest(FunctionalTest):
         # She checks her email and finds a message
         email = mail.outbox[0]
         self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(SUBJECT, email.subject)
+        self.assertEqual(email.subject, SUBJECT)
 
         # It has a url link in it
         self.assertIn('Use this link to log in', email.body)
@@ -40,11 +40,11 @@ class LoginTest(FunctionalTest):
         url = url_search.group(0)
         self.assertIn(self.server_url, url)
 
-        # She clicks it
+        # she clicks it
         print("URL: ", url)
         self.browser.get(url)
 
-        # She is logged in
+        # she is logged in!
         self.wait_for(
             lambda: self.browser.find_element_by_link_text('Log out')
         )
--- superlists/settings.py
+++ superlists/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 )
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -102,3 +108,25 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'carl.earvin.yu@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
